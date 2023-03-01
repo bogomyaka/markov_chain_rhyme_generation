7117236d6ce70ea85eb66b91cf0bf29c3087eb4b
bogomyaka <bogomyaka@hotmail.com>
2023-03-01 18:56:43 +0500

    first commit

---
+++ rhyme_generation.py
@@ -0,0 +1,42 @@
+import markovify
+import sentence_generation
+
+MAX_TRIES = 1000
+
+class MarkovModel(markovify.Text):
+    def __init__(self, **kwargs):
+        #Класс наследуется от markovify.Text,
+        #поэтому вызываем инит родительского класса с переданным 
+        #словарем параметров.
+        super().__init__(**kwargs)
+    
+    def generate_rhyme(self, total_couplets=2, max_tries=MAX_TRIES):
+        """
+        Генерирует рифму с заданным количеством двустиший и попыток.
+
+            total_couplets: количество двустиший.
+            max_tries: максимальное количество попыток
+            придумать рифму к предложению. Если оно исчерпано, 
+            то рифма ищется относительно нового сгенерированного 
+            предложения.
+        """
+        couplets = 0
+
+        while couplets != total_couplets:
+            first_sentence = sentence_generation.Sentence(self)
+            second_sentence = sentence_generation.Sentence(self)
+
+            tries = 0
+            complete_rhyme = True
+            while not any(first_sentence.end_syllables.intersection(second_sentence.end_syllables)) or first_sentence.last_word == second_sentence.last_word:
+                if tries > max_tries:
+                    complete_rhyme = False
+                    break
+                second_sentence = sentence_generation.Sentence(self)
+                tries += 1
+            
+            if complete_rhyme:
+                print(f"{first_sentence.text}\n{second_sentence.text}")
+                couplets += 1
+                if couplets % 2 == 0:
+                    print('\n')
---
+++ sentence_generation.py
@@ -0,0 +1,91 @@
+import nltk
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import cmudict
+
+MIN_WORDS = 5
+MAX_WORDS = 8
+MAX_TRIES = 1000
+STRESSES = {'0', '1', '2'}
+
+class Sentence:
+    def __init__(self, model, min_words=MIN_WORDS, max_words=MAX_WORDS, max_tries=MAX_TRIES):
+        """
+        Инициализирует созданный экзепляр класса:
+        генерирует текст предложения, а также сохраняет в атрибуты
+        последнее слово предложения и его последний слог.
+
+            model: модель, генерирующая текст.
+            min_words: минимальное количество слов в предложении.
+            max_words: максимальное количество слов в предложении.
+            max_tries: максимальное количество попыток на генерацию 
+            предложения с заданными параметрами.
+        """
+        self.text = self.generate_sentence(model, min_words, max_words, max_tries)
+        self.last_word = self.get_last_word(self.text)
+        self.end_syllables = self.get_endings(self.last_word)
+
+    def generate_sentence(self, model, min_words=MIN_WORDS, max_words=MAX_WORDS, max_tries=MAX_TRIES) -> str:
+        """
+        Генерирует предложение. 
+
+            model: модель, генерирующая текст.
+            min_words: минимальное количество слов в предложении.
+            max_words: максимальное количество слов в предложении.
+            max_tries: максимальное количество попыток на генерацию
+            предложения с заданными параметрами.
+        """
+        return model.make_sentence(min_words=MIN_WORDS, max_words=MAX_WORDS, max_tries=MAX_TRIES)
+    
+    def get_last_word(self, sentence: str) -> str:
+        """
+        Возвращает последний токен из переданной строки пропуская символы, не
+        являющиеся буквами (очень по-русски написано, да).
+
+            sentence: предложение, для которого нужно получить
+            последнее слово.
+        """
+        return RegexpTokenizer(r'\w+').tokenize(sentence)[-1]
+    
+    def get_endings(self, last_word: str) -> set:
+        """
+        Возвращает множество последних слогов слова (т.к. слово
+        может иметь несколько произношений).
+
+            last_word: слово, для которого нужно получить
+            множество последних слогов.
+        """
+        pronunciations = [pron for (word, pron) in cmudict.entries() if word == last_word]
+        endings = set()
+
+        for p in pronunciations:
+            idx = -1
+            phoneme = p[idx]
+
+            while not any(stress in phoneme for stress in STRESSES):
+                idx -= 1
+                phoneme = p[idx]
+                
+            endings.add(tuple(p[idx:]))
+
+        return endings
+    
+    def count_syllables(self, sentence: str) -> int:
+        """
+        Считает количество слогов в предложении.
+
+            sentence: предложение, для которого нужно посчитать
+            количество слогов.
+        """
+        syllables = 0
+        tokens = RegexpTokenizer(r"\w+").tokenize(sentence)
+
+        for token in tokens:
+          pronunciations = [pron for (word, pron) in cmudict.entries() if word == token]
+          if not pronunciations:
+            break
+          
+          for phoneme in pronunciations[0]:
+            if any(stress in phoneme for stress in STRESSES):
+              syllables += 1
+
+        return syllables
